ffb913e81b16586e9d6ebfc1c1c874c863d5d7db
Sean <ss56331p@pace.edu>
2026-05-10 19:06:30 -0400

    modified

--- Security.sql
+++ Security.sql
@@ -1,9 +1,14 @@
 USE titanic_db;
 
 CREATE ROLE IF NOT EXISTS 'titanic_analyst';
-CREATE USER IF NOT EXISTS 'analyst_1'@'localhost' IDENTIFIED BY 'Pass123!';
 
-GRANT 'titanic_analyst' TO 'analyst_1'@'localhost';
+CREATE USER IF NOT EXISTS 'analyst_1'@'localhost' IDENTIFIED BY 'Pass123!';
+CREATE USER IF NOT EXISTS 'analyst_2'@'localhost' IDENTIFIED BY 'Pass123!';
+
+GRANT 'titanic_analyst' TO 'analyst_1'@'localhost', 'analyst_2'@'localhost';
+
 GRANT SELECT ON titanic_db.* TO 'titanic_analyst';
 
 FLUSH PRIVILEGES;
+
+SELECT User, Host FROM mysql.user WHERE User LIKE 'analyst_%';
